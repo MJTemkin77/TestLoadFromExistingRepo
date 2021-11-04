4d382c4e143509d291a844b48861626f60e845e9
MJTemkin77 <marctemkin44@gmail.com>
2021-11-04 18:21:37 -0500

    first commit

---
+++ Assets/CubeMover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This will be a test
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
